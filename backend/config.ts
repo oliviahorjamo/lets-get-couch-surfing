cb4515e9c9a731cb0e2663535ce879a2165e264e
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-05-31 16:54:23 +0300

    Create the configurations for the different environments

---
+++ backend/config.ts
@@ -0,0 +1,34 @@
+// all the configurations
+
+// load the port
+// load the environment
+// load database url
+// if env === test, db_URI = test db url here
+// otherwise db url basic db url
+// then create a connection
+
+// export the port
+// information related to the database doesn't need to be
+// exported as you've already created the connection
+
+import * as dotenv from "dotenv";
+
+let path;
+
+switch (process.env.NODE_ENV) {
+  case "production":
+    path = `${__dirname}/.env.production`;
+    break;
+  case "test":
+    path = `${__dirname}/.env.tests`;
+    break;
+  default:
+    path = `${__dirname}/.env.development`;
+}
+
+dotenv.config({ path: path });
+
+export default {
+  PORT: process.env.PORT,
+  DB_URI: process.env.DB_URI,
+};
--- backend/src/types.ts
+++
@@ -1,3 +0,0 @@
-// here all the interfaces / other type information
-// if there are a lot of types, you could split them into
-// different modules and gather together in a folder
