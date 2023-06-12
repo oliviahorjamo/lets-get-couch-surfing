39c8fda5c7164f54680caa86d6ad0f88546975d0
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-12 18:01:04 +0300

    Write the first tests for users

--- backend/tests/user_api.test.ts
+++ backend/tests/user_api.test.ts
@@ -1,19 +1,33 @@
 import app from "../src/app";
 import supertest from "supertest";
-//import User from "../src/models/user";
-const sequelize = require("../src/db");
+import User from "../src/models/user";
+import db from "../src/db";
+const { sequelize } = db;
 
 const api = supertest(app);
 
-// Next step, fix this to create a connection to the test db
-// probs in common for all test modules
-// have a beforeEach for emptying the db
-// etc.
+const inititalUsers = [
+  {
+    name: "Tester",
+    username: "testuser",
+    password: "secret",
+  },
+];
+
+beforeEach(async () => {
+  await User.destroy({
+    where: {},
+    truncate: true,
+    cascade: true,
+  });
+  let userObject = User.build(inititalUsers[0]);
+  await userObject.save();
+});
 
 test("user route returns a list of users", async () => {
   await api.get("/api/users/").expect(200);
 });
 
-afterAll(() => {
-  sequelize.close();
+afterAll(async () => {
+  await sequelize.close();
 });
