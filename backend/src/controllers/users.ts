f719a438d608745ece38dabec3ba8f7483527140
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-16 01:10:24 +0300

    Create controller methods for creating a new user and getting all users

--- backend/src/controllers/users.ts
+++ backend/src/controllers/users.ts
@@ -1,15 +1,33 @@
 import { Request, Response } from "express";
-//import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import User from "../db/models/user";
 
 class UserController {
   async getbyId(req: Request, res: Response) {
     try {
-      console.log('reading by id');
       const { id } = req.params;
       const record = await User.findOne({ where: { id } });
-      console.log('record given', record);
       return res.json(record);
+    } catch (e) {
+      return res.json(e);
+    }
+  }
+
+  async createNew(req: Request, res: Response) {
+    const id = uuidv4();
+    try {
+      const userToCreate = {...req.body, id};
+      const record = await User.create(userToCreate);
+      return res.json({record});
+    } catch (e) {
+      return res.json(e);
+    }
+  }
+
+  async getAll(_req: Request, res: Response) {
+    try {
+      const records = await User.findAll({ where: {}});
+      return res.json(records);
     } catch (e) {
       return res.json(e);
     }
--- backend/src/routers/users.ts
+++ backend/src/routers/users.ts
@@ -1,41 +1,19 @@
-import express, { Request, Response } from 'express';
-//import userService from '../services/userService'
+import express from 'express';
 import UserValidator from '../validators/users'; 
-import { v4 as uuidv4 } from 'uuid';
 import Middleware from '../middleware';
 import UserController from '../controllers/users';
-import User from '../db/models/user';
 
 const userRouter = express.Router();
-
-// TODO
-// Type the functions correctly so that you know what they return
-// Set the middleware validation error handle in app.use()
-//  so that you don't have to write it everywhere
 
 userRouter.post('/',
   UserValidator.checkCreateUser(),
   Middleware.handleValidationError,
-  async (req: Request, res: Response) => {
-    const id = uuidv4();
-    try {
-      const userToCreate = {...req.body, id};
-      const record = await User.create(userToCreate);
-      return res.json({record});
-    } catch (e) {
-      return res.json(e);
-    }
-  }
+  UserController.createNew
 );
 
-userRouter.get('/', async (req: Request, res: Response) => {
-  try {
-    const records = await User.findAll({ where: {}});
-    return res.json(records);
-  } catch (e) {
-    return res.json(e);
-  }
-});
+userRouter.get('/', 
+  UserController.getAll
+);
 
 userRouter.get('/:id',
   UserValidator.checkIdParam(),
