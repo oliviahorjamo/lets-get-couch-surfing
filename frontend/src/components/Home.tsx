7f2f08aa67fbdcdbd4609cba2941a6d88e404502
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-19 19:17:30 +0300

    Create userReducer for initializing users in frontend

---
+++ frontend/src/components/Home.tsx
@@ -0,0 +1,12 @@
+import { useAppSelector } from "../hooks";
+
+const Home = () => {
+  const users = useAppSelector((state) => state.users);
+
+  if (!users) {
+    return <div></div>;
+  }
+  return <div>{users.map((u) => u.username)}</div>;
+};
+
+export default Home;
