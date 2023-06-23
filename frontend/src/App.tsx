385ed574991140fff3dfb69c1abcc3eeaa9de3e1
Olivia Horjamo <olivia.horjamo@gmail.com>
2023-06-23 19:08:43 +0300

    Create the very first component for signing in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,27 @@ import { useAppDispatch } from "./hooks";
 import { useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
+import SignIn from "./components/SignIn";
 import { initializeUsers } from "./reducers/userReducer";
 
 const App = () => {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    // here initialize users in the first version
-    // later no initializations here but only in components?
     dispatch(initializeUsers());
   }, [dispatch]);
 
-  // later create a router here that creates the
 
   return (
     <div>
       <div>
         <Link to="/">home</Link>
+        <Link to="/signin">sign in</Link>
       </div>
 
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<SignIn />}></Route>
       </Routes>
     </div>
   );
